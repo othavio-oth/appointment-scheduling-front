559cd1f73a6bcf2a4b2e39f81cbb60f78bfb831b
Othavio <otaviodelimasousa@gmail.com>
2024-10-07 15:48:28 -0300

    add react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
+import { BrowserRouter, Route, Router, Routes } from "react-router-dom";
 import "./App.css";
 import Register from "./components/Register";
 
 function App() {
   return (
     <div className="app w-4/6 mx-auto py-6">
-      <Register></Register>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/register" element={<Register />}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- src/components/navbar/Link.tsx
+++
@@ -1,7 +0,0 @@
-type Props = {
-  page: string;
-};
-
-const Link = ({ page }: Props) => {
-  return <div>{props}</div>;
-};
