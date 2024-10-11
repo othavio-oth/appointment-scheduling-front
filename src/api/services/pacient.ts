e0f2717f222712ea052c5633abb9dcb238d12c7f
Othavio <otaviodelimasousa@gmail.com>
2024-10-11 13:40:36 -0300

    add pacient type and methods

--- src/api/services/pacient.ts
+++ src/api/services/pacient.ts
@@ -1,12 +1,33 @@
 import axios from "axios";
-
+import { Pacient } from "../types/pacient";
 const API_URL = import.meta.env.VITE_API_URL;
-type Pacient = {
-  name: string;
-  email: string;
-  cpf: string;
-};
+
 export const registerPacient = async (pacient: Pacient) => {
   const response = await axios.post(`${API_URL}/pacients`, pacient);
+  return response.data;
+};
+
+export const createPacient = async (pacient: Pacient) => {
+  const response = await axios.post(`${API_URL}/pacients`, pacient);
+  return response.data;
+};
+
+export const getPacients = async () => {
+  const response = await axios.get(`${API_URL}/pacients`);
+  return response.data;
+};
+
+export const getPacientById = async (id: string) => {
+  const response = await axios.get(`${API_URL}/pacients/${id}`);
+  return response.data;
+};
+
+export const getPacientByEmail = async (email: string) => {
+  const response = await axios.get(`${API_URL}/pacients/email/${email}`);
+  return response.data;
+};
+
+export const getPacientByCpf = async (cpf: string) => {
+  const response = await axios.get(`${API_URL}/pacients/cpf/${cpf}`);
   return response.data;
 };
---
+++ src/api/types/pacient.ts
@@ -0,0 +1,6 @@
+export type Pacient = {
+  id?: string;
+  name: string;
+  email: string;
+  cpf: string;
+};
